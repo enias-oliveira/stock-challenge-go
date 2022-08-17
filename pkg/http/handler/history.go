3250e9f9add04ea2e362b8e9c2670943c5a21532
Enias Oliveira <eniasoliveira27@gmail.com>
2022-08-17 06:16:27 -0300

    feat: format history response

--- pkg/http/handler/history.go
+++ pkg/http/handler/history.go
@@ -12,6 +12,16 @@ type HistoryHandler struct {
 	historyService srvcInterface.HistoryService
 }
 
+type StockResponse struct {
+	Symbol string  `json:"symbol"`
+	Date   string  `json:"date"`
+	Name   string  `json:"name"`
+	Open   float32 `json:"open"`
+	High   float32 `json:"high"`
+	Low    float32 `json:"low"`
+	Close  float32 `json:"close"`
+}
+
 func NewHistoryHandler(historyService srvcInterface.HistoryService) *HistoryHandler {
 	return &HistoryHandler{historyService: historyService}
 }
@@ -37,11 +47,25 @@ func (hh *HistoryHandler) GetHistory(c *gin.Context) {
 
 	stockHistory, err := hh.historyService.FindAccountStockHistory(id)
 
+	var stockHistoryResponse []StockResponse
+
+	for _, stock := range stockHistory {
+		stockHistoryResponse = append(stockHistoryResponse, StockResponse{
+			Symbol: stock.Symbol,
+			Date:   stock.CreatedAt.UTC().String(),
+			Name:   stock.Name,
+			Open:   stock.Open,
+			High:   stock.High,
+			Low:    stock.Low,
+			Close:  stock.Close,
+		})
+	}
+
 	if err != nil {
 		c.JSON(500, gin.H{
 			"message": "error",
 		})
 	}
 
-	c.JSON(200, stockHistory)
+	c.JSON(200, stockHistoryResponse)
 }
