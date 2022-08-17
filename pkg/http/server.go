8720bec48dfc3b3db800222ba6dbf04353b61425
Enias Oliveira <eniasoliveira27@gmail.com>
2022-08-17 01:05:02 -0300

    feat(wip): adds static stock fetching from stooq

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"io/ioutil"
+	"net/http"
 	"stock-challenge-go/pkg/http/handler"
 	"stock-challenge-go/pkg/http/middleware"
 
@@ -29,6 +31,35 @@ func NewServerHTTP(accountHandler *handler.AccountHandler) *ServerHTTP {
 	api := engine.Group("/api", middleware.AuthorizationMiddleware)
 
 	api.GET("/profile", accountHandler.Profile)
+
+	api.GET("/stock", func(c *gin.Context) {
+		quote := c.Query("q")
+
+		requestURL := "https://stooq.com/q/l/?s=" + quote + "&f=sd2t2ohlcvn&h&e=csv"
+
+		res, err := http.Get(requestURL)
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"quote": quote,
+			"data":  string(body),
+		})
+	})
 
 	return &ServerHTTP{engine: engine}
 }
